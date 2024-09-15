cbefeafc75abc40b7e076348d1ff39ad26a9bee3
ByteBrewer101 <atharvachopde31@gmail.com>
2024-09-15 21:15:49 +0530

    design2

--- app/pages/chatpage/page.tsx
+++ app/pages/chatpage/page.tsx
@@ -1,5 +1,7 @@
 export default function ChatPage(){
-    return <div>
-        ChatPage
+    return <div className="h-screen">
+        <div className="h-full bg-red-500">
+
+        </div>
     </div>
 }
