dcee1c99b24a9b480f26e8d61705484ee8430fc1
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-13 22:08:31 +0530

    user card

--- app/pages/chatpage/page.tsx
+++ app/pages/chatpage/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { ChatComp } from "@/app/components/ChatComponent";
+import { UserData } from "@/app/components/Userdatacard";
 
 
 export default function ChatPage(){
@@ -7,7 +8,8 @@ export default function ChatPage(){
 
 
     return (
-      <div className="flex justify-center items-center w-full h-screen p-4 py-20 md:p-20  bg-graph-paper ">
+      <div className="flex justify-center items-start space-x-4 w-full h-screen p-4 py-20 md:p-20  bg-graph-paper ">
+        <UserData/>
         <ChatComp />
       </div>
     );
