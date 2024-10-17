11304793f0b7c12fd503d36ade6bbec6b44be33b
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-17 23:09:02 +0530

    c1

--- app/components/Userdatacard.tsx
+++ app/components/Userdatacard.tsx
@@ -11,7 +11,7 @@ export function UserData() {
           </Avatar>
           <div className="flex flex-col">
             <h1>Name Lastname</h1>
-            <h1>position</h1>
+            <h1>position company</h1>
           </div>
         </div>
         <div className="flex items-center space-x-2 bg-white bg-opacity-10 rounded-xl p-2  justify-start">
