f60bc3e7d29c49c35c3516fd603ec84c93406fe1
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-27 05:33:50 +0530

    latest

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,12 @@ import jwt from "jsonwebtoken"; // Ensure this is installed
 const handler = NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-expect-error
+      clientId: process.env.GITHUB_ID,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
@@ -17,23 +21,22 @@ const handler = NextAuth({
         console.log(user);
         token.urlfile = jwt.sign(
           { user },
-          process.env.JWT_SECRET || "secret", // Use a secure secret
-         
+          process.env.JWT_SECRET || "secret" // Use a secure secret
         );
 
         console.log(token);
       }
-      
+
       return token;
     },
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    async session({ session, token }:any) {
+    async session({ session, token }: any) {
       // Ensure the token has the jwt property set
-      
-        session.accessToken = token.urlfile as string // Set the JWT token to session.accessToken
-    
-    console.log(session.accessToken);
+
+      session.accessToken = token.urlfile as string; // Set the JWT token to session.accessToken
+
+      console.log(session.accessToken);
       return session;
     },
   },
