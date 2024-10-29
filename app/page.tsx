49819ee96a075efb4f9e61966de0cb4546219e64
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-29 22:13:51 +0530

    moved websocket conn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 
 import TopBar from "./components/Topbar";
-import { useWebSocketServer } from "./customHooks/Connection";
+
 
 import AppPage from "./screens/AppPage";
 
 
 export default function Home() {
 
-   const url = process.env.NEXT_PUBLIC_API_URL || "";
-   useWebSocketServer(url);
+
  
 
   return <div>
--- app/recoilrootprovider.tsx
+++ app/recoilrootprovider.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { RecoilRoot } from "recoil";
-
-
+import { useWebSocketServer } from "./customHooks/Connection";
 
 export default function RecoilContextProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const url = process.env.NEXT_PUBLIC_API_URL || "";
+  useWebSocketServer(url);
+
   return (
     <RecoilRoot>
       <ConnectionInitializer>{children}</ConnectionInitializer>
@@ -17,8 +19,6 @@ export default function RecoilContextProvider({
 }
 
 function ConnectionInitializer({ children }: { children: React.ReactNode }) {
-
-
   // Corrected to return JSX
   return <>{children}</>;
 }
